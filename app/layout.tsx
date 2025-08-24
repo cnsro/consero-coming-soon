197b87ce1c13d81d7c607b8208cf8507475c86a3
Freddie <consero.collection@outlook.com>
2025-08-24 15:26:12 +0100

    vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@
 import type { Metadata } from 'next';
 import { Inter, Cormorant_Garamond } from 'next/font/google';
 import './globals.css';
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({ 
   subsets: ['latin'],
